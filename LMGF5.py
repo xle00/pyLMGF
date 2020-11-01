851f4b69b8d968160d7bf4a93cfdc61652df0989
xle00 <34048656+xle00@users.noreply.github.com>
2020-11-01 19:22:08 -0300

    Update LMGF5.py

--- LMGF5.py
+++ LMGF5.py
@@ -2,17 +2,57 @@ import time
 from Mouse import Mouse
 from Process import ProcessMemory, ProcessWindow
 from configs import Pointers, QuestDB
+from mss import mss
+import ctypes
+
+sct = mss()
+
+
+def get_pixel_brightness(x, y):
+    area = {"left": x, "top": y, "width": 1, "height": 1}
+    img = sct.grab(area)
+    # noinspection PyTypeChecker
+    pixel = list(img.pixels[0][0])
+    return sum(pixel) / len(pixel)
 
 
 class Slot:
-    def __init__(self, number, scroll, x, y):
+    details = {
+        1: {'scroll': 0, 'pixel_x': 472, 'pixel_y': 383},
+        2: {'scroll': 0, 'pixel_x': 656, 'pixel_y': 383},
+        3: {'scroll': 0, 'pixel_x': 291, 'pixel_y': 636},
+        4: {'scroll': 0, 'pixel_x': 472, 'pixel_y': 636},
+        5: {'scroll': 0, 'pixel_x': 656, 'pixel_y': 636},
+        6: {'scroll': 8, 'pixel_x': 291, 'pixel_y': 614},
+        7: {'scroll': 8, 'pixel_x': 472, 'pixel_y': 614},
+        8: {'scroll': 8, 'pixel_x': 656, 'pixel_y': 614},
+        9: {'scroll': 16, 'pixel_x': 291, 'pixel_y': 596},
+        10: {'scroll': 16, 'pixel_x': 472, 'pixel_y': 596},
+        11: {'scroll': 16, 'pixel_x': 656, 'pixel_y': 596},
+        12: {'scroll': 24, 'pixel_x': 291, 'pixel_y': 580},
+        13: {'scroll': 24, 'pixel_x': 472, 'pixel_y': 580},
+        14: {'scroll': 24, 'pixel_x': 656, 'pixel_y': 580},
+        15: {'scroll': 32, 'pixel_x': 291, 'pixel_y': 563},
+        16: {'scroll': 32, 'pixel_x': 472, 'pixel_y': 563},
+        17: {'scroll': 32, 'pixel_x': 656, 'pixel_y': 563},
+        18: {'scroll': 40, 'pixel_x': 291, 'pixel_y': 547},
+        19: {'scroll': 40, 'pixel_x': 472, 'pixel_y': 547},
+        20: {'scroll': 40, 'pixel_x': 656, 'pixel_y': 547},
+    }
+
+    def __init__(self, number):
         self.number = number
-        self.scroll = scroll
-        self.x = x
-        self.y = y
+        d = Slot.details.get(number)
+
+        self.scroll = d.get('scroll')
+        self.x = d.get('pixel_x')
+        self.y = d.get('pixel_y')
 
         self.timer = None
         self.qid = None
+
+    def __repr__(self):
+        return f'Slot: {self.number} | x: {self.x} | y: {self.y} | scroll: {self.scroll}'
 
 
 class MemoryReader(ProcessMemory):
@@ -20,12 +60,117 @@ class MemoryReader(ProcessMemory):
         super(MemoryReader, self).__init__('Lords Mobile.exe')
         self.pointers = Pointers()
 
+    def get_clock(self):
+        # get pointer
+        module, base_pointer, pointers_ = self.pointers.get_pointer_by_name('clock')
+
+        # read value at address
+        module_addr = self.get_module_address_by_name(module)
+        address = self.get_pointer(module_addr + base_pointer, pointers_)
+        value = self.read_4_bytes(address)
+
+        return value
+
     def get_active_timer(self):
-        pass
+        # get pointer
+        module, base_pointer, pointers_ = self.pointers.get_pointer_by_name('active_time')
+
+        # read value at address
+        module_addr = self.get_module_address_by_name(module)
+        address = self.get_pointer(module_addr + base_pointer, pointers_)
+        value = self.read_string(address, 5)
+
+        # convert value to int (seconds)
+        minutes, seconds = value.split(':')
+        timer = int(minutes)*60 + int(seconds)
+
+        return timer
+
+    def get_mission_details(self):
+        """{pointer_name: (string_length, offset)}"""
+        pointers = {'quest_points': (4, 1), 'quest_requirements': (20, 4), 'quest_time': (11, 0)}
+
+        result = []
+        for name, values in pointers.items():
+            # get pointer
+            module, base_pointer, pointers_ = self.pointers.get_pointer_by_name(name)
+
+            # read string at address
+            module_addr = self.get_module_address_by_name(module)
+            addr = self.get_pointer(module_addr + base_pointer, pointers_)
+            value = self.read_string(addr+values[1], values[0])
+            result.append(value)
+
+        return result
+
+    def get_player_name(self):
+        # get pointer
+        module, base_pointer, pointers_ = self.pointers.get_pointer_by_name('player_name')
+
+        # get value at address
+        module_addr = self.get_module_address_by_name(module)
+        addr = self.get_pointer(module_addr + base_pointer, pointers_)
+        value = self.read_string(addr, 12)
+
+        return value
+
+
+class GuildFest:
+    def __init__(self):
+        self.slots = [Slot(number) for number in Slot.details.keys()]
+
+        self.window = ProcessWindow('UnityWndClass', 'Lords Mobile')
+        self.memory = MemoryReader()
+        self.db = QuestDB()
+
+        self.current_scroll = 0
+
+    def get_board(self):
+        for slot in self.slots:
+            self.scroll(slot.scroll)
+            Mouse.left_click(self.window.x + slot.x, self.window.y + slot.y)
+
+            time.sleep(.1)
+            if self.is_quest():
+                slot.timer = None
+                slot.target = None
+                slot.qid = self.identify_quest()
+                print(slot.qid)
+            else:
+                timer, clock = self.memory.get_active_timer(), self.memory.get_clock()
+                print(timer)
+                slot.timer = timer
+                slot.target = clock + timer
+                slot.qid = None
+
+
+        self.scroll(0)
+
+    def identify_quest(self):
+        details = self.memory.get_mission_details()
+        return self.db.identify_quest(*details)
+
+
+    def scroll(self, clicks):
+        Mouse.wheel(clicks - self.current_scroll, self.window.x + 303, self.window.y + 587, .008)
+        self.current_scroll = clicks if clicks > 0 else 0
+
+    def is_quest(self):
+        brightness = get_pixel_brightness(self.window.x + 885, self.window.y + 354)
+        if brightness < 127:
+            return False
+        return True
+
+
+
+
 
 
 def main():
-    pass
+    fg = GuildFest()
+    fg.window.activate()
+    fg.get_board()
+
 
 
 
