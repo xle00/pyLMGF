91918e81328cde4c7a316bcf44f0f83db7201215
xle00 <34048656+xle00@users.noreply.github.com>
2020-11-01 17:16:18 -0300

    Update LMGF5.py

--- LMGF5.py
+++ LMGF5.py
@@ -1,2 +1,33 @@
 import time
 from Mouse import Mouse
+from Process import ProcessMemory, ProcessWindow
+from configs import Pointers, QuestDB
+
+
+class Slot:
+    def __init__(self, number, scroll, x, y):
+        self.number = number
+        self.scroll = scroll
+        self.x = x
+        self.y = y
+
+        self.timer = None
+        self.qid = None
+
+
+class MemoryReader(ProcessMemory):
+    def __init__(self):
+        super(MemoryReader, self).__init__('Lords Mobile.exe')
+        self.pointers = Pointers()
+
+    def get_active_timer(self):
+        pass
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
